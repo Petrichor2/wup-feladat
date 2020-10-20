9a169044ec858b4f3c585e5f88237bd78a229905
Dániel Fekete <fekete.dani1994@gmail.com>
2020-10-20 03:36:48 +0200

    Added Dockerfiles

--- library-impl/src/main/java/com/wup/felveteli/library/SpringFoxConfig.java
+++ library-impl/src/main/java/com/wup/felveteli/library/SpringFoxConfig.java
@@ -5,8 +5,12 @@ import com.google.common.collect.Sets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,14 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
-
-    @Value("${server.address:127.0.0.1}")
-    private String address;
-
-    @Value("${server.port:8080}")
-    private Integer port;
-
+public class SpringFoxConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -30,8 +27,22 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .host(address + ":" + port)
                 .protocols(Sets.newHashSet("http", "https"))
                 .securitySchemes(Lists.newArrayList(new ApiKey("api_key", "api_key", "header")));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("REST API")
+                .description("Servicesx")
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
